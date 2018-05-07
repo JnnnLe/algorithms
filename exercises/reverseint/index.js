d9af2e8ed05f7b4cd512925daf2bf4094d1da672
Jennifer Le <jnnnle.sf@gmail.com>
2018-05-07 13:30:26 -0700

    reverseInt soln

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  //split up the int
+  //check for trailing zeros => parseInt will innately do this
+  //check for negative negative sign
+  //return the reversed int, must be int
+
+  let reversed = [];
+  let strInt = n.toString().split('');
+
+  if (Math.sign(n) === -1) {
+    for (let i = strInt.length - 1; i > 0; i--) {
+      reversed.push(strInt[i]);
+    }
+    return - (parseInt(reversed.join('')));
+  }
+
+  
+  for (let i = strInt.length - 1; i >= 0; i--) {
+    reversed.push(strInt[i]);
+  }
+  return (parseInt(reversed.join('')));
+}
 
 module.exports = reverseInt;
