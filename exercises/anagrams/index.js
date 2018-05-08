8a706dbc77b3c7ecab83d6c29ae556cd1a4800d0
Jennifer Le <jnnnle.sf@gmail.com>
2018-05-07 21:16:21 -0700

    2 Anagram solns

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
 
+// //helper fn
+// function buildChar(str) {
+//   const charMap = {};
+
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap;
+// }
+
+// function anagrams(stringA, stringB) {
+//   // disreguard spaces, capitol letters, and symbols
+//   // word.replace(/['\w], '')
+//   const aChar = buildChar(stringA);
+//   const bChar = buildChar(stringB);
+
+//   if (Object.keys(aChar).length !== Object.keys(bChar).length) {
+//     return false;
+//   } 
+
+//   for (let char in aChar) {
+//     if (aChar[char] !== bChar[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase()
+                                  .split('')
+                                  .sort()
+                                  .join('')
+}
+
+function anagrams(strA, strB) {
+  return cleanString(strA) ===cleanString(strB);
+}
+
+
+ 
 module.exports = anagrams;
