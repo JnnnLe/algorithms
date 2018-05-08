5d8bc64aac4b26123abbfbdd20f93dc536f96509
Jennifer Le <jnnnle.sf@gmail.com>
2018-05-07 22:11:11 -0700

    3 Capitalize solns

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,47 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+
+  const capWords = [];
+
+  let strArray = str.split(' ');
+
+  strArray.forEach((word, i) => {
+    let upperLetter = strArray[i][0].toUpperCase() + word.slice(1)
+    capWords.push(upperLetter)
+
+  })
+
+  return capWords.join(' ');
+}
+
+
+// function capitalize(str) {
+  
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       resuit += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+
+//   return result;
+// }
+
+
+// function capitalize(str) {
+
+//   const capWords = [];
+
+//   for (let word of str.split(' ')) {
+//     capWords.push(word[0].toUpperCase() + word.slice(1));
+//   }
+
+//   return capWords.join(' ');
+// }
 
 module.exports = capitalize;
