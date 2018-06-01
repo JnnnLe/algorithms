113d61b7589fa9a012b1d6c7c93066b807dd5da5
J. Lê <jenn@Js-MacBook-Pro.local>
2018-06-01 16:50:18 -0700

    Insert first node to linked list(ll)

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -1,3 +1,34 @@
 // --- Directions
 // Implement classes Node and Linked Lists
 // See 'directions' document
+
+class Node {
+
+  constructor (data, next) {
+    this.data = data;
+    this.next = next || null
+  }
+}
+
+class LinkedList {
+
+  constructor () {
+    this.head = null;
+  }
+
+  insertFirst (data) {
+    //create new head
+    //if head already exists, new head's next will be prev head
+    this.head = new Node(data, this.head);
+  }
+
+  size () {
+    
+  }
+
+
+
+  //ends LinkedList  
+}
+
+module.exports = { Node, LinkedList };
