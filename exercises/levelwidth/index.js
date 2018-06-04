07e1b686b342a22a7f2724b4c0fbbe8b34a92b56
J. Lê <jenn@Js-MacBook-Pro.local>
2018-06-04 15:30:03 -0700

    Test commit

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+
+  //use BFT for a node in this problem
+  let counters = [0];
+
+  const queue = [root, 'stop'];
+
+  while (queue.length > 1) {
+    const node = queue.shift();
+
+    if (node === 'stop') {
+      counters.push(0);
+      queue.push('stop');
+    } else {
+      queue.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
 
 module.exports = levelWidth;
